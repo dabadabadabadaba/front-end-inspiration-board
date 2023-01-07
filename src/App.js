3eaab82ded8bb796c2080ff0cc32ffae5828757c
Dominique <dominiquedaba@gmail.com>
2023-01-06 17:54:46 -0800

    refactored updateLikes with axios, added useEffect

--- src/App.js
+++ src/App.js
@@ -1,62 +1,98 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Board from "./components/Board.js";
 import NewBoardForm from "./components/NewBoardForm.js";
 import NewCardForm from "./components/NewCardForm.js";
+import axios from "axios";
 
-const INITIAL_BOARDS = [
-  {
-    boardId: 1,
-    owner: "Owner name",
-    title: "Board title",
-    card: [],
-  },
-  {
-    boardId: 2,
-    owner: "Another owner name",
-    title: "Second board title",
-    card: [],
-  },
-];
+// const INITIAL_BOARDS = [
+//   {
+//     boardId: 1,
+//     owner: "Owner name",
+//     title: "Board title",
+//     card: [],
+//   },
+//   {
+//     boardId: 2,
+//     owner: "Another owner name",
+//     title: "Second board title",
+//     card: [],
+//   },
+// ];
 
-const INITIAL_CARDS = [
-  {
-    cardId: 1,
-    message: "Card text",
-    likes_count: 0,
-  },
-  {
-    cardId: 2,
-    message: "Another card text",
-    likes_count: 90,
-  },
-];
+// const INITIAL_CARDS = [
+//   {
+//     cardId: 1,
+//     message: "Card text",
+//     likes_count: 0,
+//   },
+//   {
+//     cardId: 2,
+//     message: "Another card text",
+//     likes_count: 90,
+//   },
+// ];
 
 function App() {
-  const initialCardCopy = INITIAL_CARDS.map((card) => {
-    return { ...card };
-  });
+  // const initialBoardCopy = INITIAL_BOARDS.map((board) => {
+  //   return { ...board };
+  // });
+  // const initialCardCopy = INITIAL_CARDS.map((card) => {
+  //   return { ...card };
+  // });
 
-  const [cardData, setCardData] = useState(initialCardCopy);
+  const [cardData, setCardData] = useState([]);
+  const [boardData, setBoardData] = useState([]);
+
+  const URL = "http://localhost:5000/board";
+
+  // const getAllBoards = ()
+
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        console.log("Calling API");
+        const boardsAPICopy = response.data.map((board) => {
+          return {
+            boardId: board.board_id,
+            owner: board.owner,
+            title: board.title,
+            cards: board.cards_list,
+          };
+        });
+        setBoardData(boardsAPICopy);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
 
   //Need to make a patch request to database (when ready)
   const updateLikes = (cardId, updatedLikes) => {
     console.log("updateLikes called");
     const newCardsList = [];
-    for (const card of cardData) {
-      console.log(`Card.id is ${card.id} and cardId is ${cardId}`);
-      if (card.cardId !== cardId) {
-        newCardsList.push(card);
-      } else {
-        const newCard = {
-          ...card,
-          likes_count: updatedLikes,
-        };
-        console.log(newCard);
-        newCardsList.push(newCard);
-      }
-    }
-    setCardData(newCardsList);
+    axios
+      .patch(`${URL}/${cardId}`)
+      .then((response) => {
+        for (const card of cardData) {
+          console.log(`Card.id is ${card.id} and cardId is ${cardId}`);
+          if (card.cardId !== cardId) {
+            newCardsList.push(card);
+          } else {
+            const newCard = {
+              ...card,
+              likes_count: updatedLikes,
+            };
+            console.log(newCard);
+            newCardsList.push(newCard);
+          }
+        }
+        setCardData(newCardsList);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   };
 
   //Function to create new card
@@ -73,13 +109,7 @@ function App() {
     setCardData(newCardList);
   };
 
-  const initialBoardCopy = INITIAL_BOARDS.map((board) => {
-    return { ...board };
-  });
-
-  const [boardData, setBoardData] = useState(initialBoardCopy);
-
-  // Function to create new board
+  // Function to create new board, needs POST with axios
   const addBoard = (newBoardInfo) => {
     const newBoardList = [...boardData];
 
